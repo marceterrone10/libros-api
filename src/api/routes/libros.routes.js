bd6815f3feab253ad9a313cc833f63beeef6975c
marceterrone10 <marcelo.terrone10@gmail.com>
2025-07-15 17:49:33 -0300

    Validación con el middleware de autentificación para POST, PUT Y DELETE

--- src/api/routes/libros.routes.js
+++ src/api/routes/libros.routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { obtenerLibros, obtenerLibroPorId, crearLibro, actualizarLibro, eliminarLibro } from '../controllers/libros.controllers.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
+
 
 const router = Router();
 
 router.get('/', obtenerLibros); // Define la ruta para obtener todos los libros
 router.get('/:id', obtenerLibroPorId); // Define la ruta para obtener un libro por ID
-router.post('/', crearLibro); // Define la ruta para crear un nuevo libro
-router.put('/:id', actualizarLibro); // Define la ruta para actualizar un libro por ID y ISBN
-router.delete('/:id', eliminarLibro); // Define la ruta para eliminar un libro por ID
-export default router; 
+router.post('/', authMiddleware, crearLibro); // Define la ruta para crear un nuevo libro
+router.put('/:id', authMiddleware, actualizarLibro); // Define la ruta para actualizar un libro por ID y ISBN
+router.delete('/:id', authMiddleware, eliminarLibro); // Define la ruta para eliminar un libro por ID
+
+// Solo usuarios registrados y logeados pueden postear, actualizar o eliminar libros
+
+export default router;
--- src/api/routes/usuario.routes.js
+++ src/api/routes/usuario.routes.js
@@ -4,9 +4,15 @@ import { authMiddleware } from '../middlewares/auth.middleware.js';
 const router = Router();
 
 router.get('/perfil', authMiddleware, (req, res) => {
+
+    const { id, email } = req.user; // importante para obtener el ID y email del usuario autenticado, user viene de authMiddleware cuando decodeamos
+
     res.json({
-        usuario: req.usuario,
-        message: 'Perfil de usuario obtenido correctamente'
+        message: 'Perfil de usuario obtenido correctamente',
+        usuario: {
+            id,
+            email
+        }
     });
 });
 
